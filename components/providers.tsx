c1630ab04c8eeec382c834983e1d0369408dc1fc
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-03 20:44:11 +0600

    feat: implement tast container in providers.tsx file

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,11 +2,16 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ToastContainer position="bottom-right" autoClose={3000} />
+    </QueryClientProvider>
   );
 }
