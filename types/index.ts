4f89d81d5bc926b49a32578490fb801389357a4b
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-23 11:02:36 +0600

    added assesment value on certificate interface

--- types/index.ts
+++ types/index.ts
@@ -37,14 +37,14 @@ export interface ExperienceItem {
   current?: boolean;
 }
 
-export interface Certificate {
+type Certificate = {
   title: string;
   issuer: string;
   year: string;
   code: string;
-  type: "Certificate" | "Learning";
+  type: "Certificate" | "Assessment" | "Learning";
   url: string;
-}
+};
 
 export interface ServiceItem {
   index: string;
